1a5abf0db00e8a0c06d9d96fbd1d80c6181894f9
Diego Fernando Jojoa <91098381+diegojojoayandun@users.noreply.github.com>
2023-01-24 07:04:43 -0500

    Add content to README

---
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -0,0 +1,13 @@
+process.stdout.write("Welcome to Holberton School, what is your name?\n");
+
+process.stdin.on("readable", () => {
+  const userName = process.stdin.read();
+
+  if (userName) {
+    process.stdout.write(`Your name is: ${userName}`);
+  }
+});
+
+process.stdin.on("close", () => {
+  process.stdout.write("This important software is now closing\n");
+});
